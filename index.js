976f85d0071db30dac8734e344be513fc48cb518
bkevin1052 <31713984+bkevin1052@users.noreply.github.com>
2021-03-18 22:10:38 -0600

    agregando middlewares

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
-const express = require('express')
+const express = require('express');
+const morgan = require('morgan');
+const multer = require('multer');
+const path = require('path');
 
 //Inicializacion
-
 const app = express();
 
 //Settings
-
 app.set('port',3000);
 
-// Start Server
+//Middlewares
+app.use(morgan('dev'));
 
+const storage = multer.diskStorage({
+    destination: path.join(__dirname,'public/uploads'),
+    filename(req,file,cb){
+        cb(null,new Date().getTime()+ path.extname(file.originalname));
+    }
+})
+
+app.use(multer({storage}).single('image'));
+
+app.use(express.urlencoded({extended:false}));
+
+app.use(express.json());
+// Start Server
 app.listen(app.get('port'),()=>{
 
     console.log('Server on port',app.get('port'));
