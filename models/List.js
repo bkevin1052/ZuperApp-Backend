c7017768a66013d7d5d96f7c42fd009bab24b0c7
bkevin1052 <31713984+bkevin1052@users.noreply.github.com>
2021-04-26 13:14:16 -0600

    agregando lists

--- models/List.js
+++ models/List.js
@@ -3,8 +3,8 @@ const { Schema, model } = require('mongoose');
 const ListSchema = new Schema({
     username: { type: String, required: true },
     namelist: { type: String, required: true },
-    description: { type: String, required: true },
-    items: { type: String, required: true },
+    description: { type: String},
+    items: { type: Array,  default:[]},
     create_at: {type: Date, default: Date.now }
 });
 
--- routes/lists.js
+++ routes/lists.js
@@ -4,22 +4,33 @@ const router = Router();
 const path = require('path');
 const { unlink } = require('fs-extra');
 
-
-
 const List = require('../models/List');
 
-router.get('/', async (req, res) => {
-    const lists = await List.find().sort('-_id');
-    res.json(lists);
-});
+router.post('/createlist', async (req, res) => {
 
-
-router.post('/', async (req, res) => {
-    const { username, namelist, description,items} = req.body;
-    const newList = new List({ username, namelist, description,items});
+    var username = req.body.username;
+    var namelist = req.body.name;
+    var description = req.body.description;
+    const newList = new List({ username, namelist, description});
     console.log(newList)
     await newList.save();
-    res.json({'message': 'List Saved'});
+    res.json({token: '', username: username,mensaje: "Lista registrada correctamente!",codigo: '100'});
+    res.end();
 });
+
+router.post('/editlist', async (req, res) => {
+
+    let doc = await List.findOneAndUpdate({username:req.body.username,namelist:req.body.name }, {namelist:req.body.name, description:req.body.description,items:req.body.items});
+    res.json({token: '', username: req.body.username,mensaje: "Lista actualizada correctamente!",codigo: '100'});
+    res.end();
+});
+
+router.post('/getlists', async (req, res) => {
+
+    const lists = await List.find({username:req.body.username});
+    res.json(lists);
+    res.end();
+});
+
 
 module.exports = router;
