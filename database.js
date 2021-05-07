74686abd7c3e553b2dde47d6ad9055bf292b00d0
bkevin1052 <31713984+bkevin1052@users.noreply.github.com>
2021-05-07 15:02:33 -0600

    Agrengando parametros en connection de BD

--- database.js
+++ database.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb+srv://kevin:Magicjack123@cluster0.ltzut.mongodb.net/ZuperAppDB?retryWrites=true&w=majority', {
-    useNewUrlParser: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true    
 }) 
     .then(db => console.log(`DB is connected`))
     .catch(err => console.error(err));
